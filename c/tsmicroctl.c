8584d8f7e54586a90840c14c61baf104a1d205f1
Kris Bahnsen <kris@embeddedarm.com>
2017-04-19 15:08:03 -0700

    Added proper scaling and resistor math to ADC output for voltage rails

--- c/tsmicroctl.c
+++ c/tsmicroctl.c
@@ -1,4 +1,4 @@
-/*  Copyright 2016, Unpublished Work of Technologic Systems
+/*  Copyright 2017, Unpublished Work of Technologic Systems
 *  All Rights Reserved.
 *
 *  THIS WORK IS AN UNPUBLISHED WORK AND CONTAINS CONFIDENTIAL,
@@ -19,7 +19,7 @@
 /* To compile tshwctl, use the appropriate cross compiler and run the
 * command:
 *
-*  gcc -fno-tree-cselim -Wall -O0 -mcpu=arm9 -DCTL -o tsmicroctl tsmicroctl.c
+*  arm-linux-gnueabihf-gcc -fno-tree-cselim -Wall -O0 -DCTL -o tsmicroctl tsmicroctl.c
 *
 */
 
@@ -49,14 +49,13 @@ int get_model()
 	FILE *proc;
 	char mdl[256];
 	char *ptr;
-	int sz;
 
 	proc = fopen("/proc/device-tree/model", "r");
 	if (!proc) {
 	    perror("model");
 	    return 0;
 	}
-	sz = fread(mdl, 256, 1, proc);
+	fread(mdl, 256, 1, proc);
 	ptr = strstr(mdl, "TS-");
 	return strtoull(ptr+3, NULL, 16);
 }
@@ -67,8 +66,8 @@ int silabs_init()
 	static int fd = -1;
 	fd = open("/dev/i2c-0", O_RDWR);
 	if(fd != -1) {
-		if (ioctl(fd, I2C_SLAVE_FORCE, 0x78) < 0) {
-			perror("Microcontroller did not ACK 0x78\n");
+		if (ioctl(fd, I2C_SLAVE_FORCE, 0x2a) < 0) {
+			perror("Microcontroller did not ACK 0x2a\n");
 			return -1;
 		}
 	}
@@ -78,29 +77,51 @@ int silabs_init()
 
 #ifdef CTL
 
+// Scale voltage to silabs 0-2.5V
+uint16_t inline sscale(uint16_t data){
+	return data * (2.5/1023) * 1000;
+}
+
+// Scale voltage for resistor dividers
+uint16_t inline rscale(uint16_t data, uint16_t r1, uint16_t r2)
+{
+	uint16_t ret = (data * (r1 + r2)/r2);
+	return sscale(ret);
+}
+
 void do_info(int twifd)
 {
-	uint8_t data[28];
-	bzero(data, 28);
-	read(twifd, data, 28);
+	uint8_t data[32];
+	unsigned int pct;
+	bzero(data, 32);
+	read(twifd, data, 32);
 
-	printf("revision=0x%x\n", (data[16] >> 4) & 0xF);
-	printf("P1_2=0x%x\n", data[0]<<8|data[1]);
-	printf("P1_3=0x%x\n", data[2]<<8|data[3]);
-	printf("P1_4=0x%x\n", data[4]<<8|data[5]);
+	printf("revision=0x%x\n", data[31]);
+	printf("SUPERCAP_V=%d\n", sscale((data[0]<<8|data[1])));
+	printf("SUPERCAP_TOT=%d\n", rscale((data[2]<<8|data[3]), 20, 20));
+	pct = (((data[2]<<8|data[3])*100/237));
+	if (pct > 311) {
+		pct = pct - 311;
+	} else {
+		pct = 0;
+	}
+	printf("SUPERCAP_PCT=%d\n", pct > 100 ? 100 : pct);
 
-	printf("P2_0=0x%x\n", data[6]<<8|data[7]);
-	printf("P2_1=0x%x\n", data[8]<<8|data[9]);
-	printf("P2_2=0x%x\n", data[10]<<8|data[11]);
-	printf("P2_3=0x%x\n", data[12]<<8|data[13]);
-	printf("P2_4=0x%x\n", data[18]<<8|data[19]);
-	printf("P2_5=0x%x\n", data[20]<<8|data[21]);
-	printf("P2_6=0x%x\n", data[22]<<8|data[23]);
-	printf("P2_7=0x%x\n", data[24]<<8|data[25]);
+	printf("VCHARGE=%d\n", rscale((data[16]<<8|data[17]), 422, 422));
+	printf("V4P7=%d\n", rscale((data[4]<<8|data[5]), 20, 20));
 
-	printf("temp_sensor=0x%x\n", data[26]<<8|data[27]);
+	printf("VIN=%d\n", rscale((data[6]<<8|data[7]), 1910, 172));
+	printf("V5_A=%d\n", rscale((data[8]<<8|data[9]), 536, 422));
+	printf("V3P3=%d\n", rscale((data[10]<<8|data[11]), 422, 422));
+	printf("RAM_V1P35=%d\n", sscale((data[12]<<8|data[13])));
+	printf("VCORE=%d\n", sscale((data[14]<<8|data[15])));
+	printf("VSOC=%d\n", sscale((data[18]<<8|data[19])));
+	printf("VARM=%d\n", sscale((data[20]<<8|data[21])));
+
+	printf("temp_sensor=0x%x\n", data[22]<<8|data[23]);
 	printf("reboot_source=");
-	switch(data[15] & 0x7) {
+
+	switch(data[30] & 0x3) {
 	  case 0:
 		printf("poweron\n");
 		break;
@@ -108,13 +129,10 @@ void do_info(int twifd)
 		printf("WDT\n");
 		break;
 	  case 2:
-		printf("resetswitch\n");
-		break;
-	  case 3:
 		printf("sleep\n");
 		break;
-	  case 4:
-		printf("brownout\n");
+	  default:
+		printf("unknown\n");
 		break;
 	}
 
@@ -126,12 +144,9 @@ static void usage(char **argv) {
 	  "Technologic Systems Microcontroller Access\n"
 	  "\n"
 	  "  -i, --info              Get info about the microcontroller\n"
-	  "  -L, --sleep             Sleep CPU, must specify -M|-m to wake\n"
-	  /*"  -F, --standby           Standby CPU, must specify -M|-m to wake\n"*/
-	  "  -M, --timewkup=<time>   Time in seconds to wake up after\n"
-	  "  -m, --resetswitchwkup   Wake up at reset switch is press\n"
-	  "  -X, --resetswitchon     Enable reset switch\n"
-	  "  -Y, --resetswitchoff    Disable reset switch\n"
+	  "  -L, --sleep=<time>      Sleep CPU, <time> seconds to wake up in\n"
+	  "  -S, --supercapon        Enable charging of TS-SILO supercaps\n"
+	  "  -s, --supercapoff       Disable charging of TS-SILO supercaps\n"
 	  "  -h, --help              This message\n",
 	  argv[0]
 	);
@@ -141,17 +156,13 @@ int main(int argc, char **argv)
 {
 	int c;
 	int twifd;
-	int opt_resetswitch = 0, opt_sleepmode = 0, opt_timewkup = 0xffffff;
-	int opt_resetswitchwkup = 0;
+	int opt_timewkup = 0xffffff, opt_supercap = 0, opt_sleep = 0;;
 
 	static struct option long_options[] = {
 	  { "info", 0, 0, 'i' },
-	  { "sleep", 0, 0, 'L'},
-	  /*{ "standby", 0, 0, 'F'},*/
-	  { "timewkup", 1, 0, 'M'},
-	  { "resetswitchwkup", 0, 0, 'm'},
-	  { "resetswitchon", 0, 0, 'X'},
-	  { "resetswitchoff", 0, 0, 'Y'},
+	  { "sleep", 1, 0, 'L'},
+	  { "supercapon", 0, 0, 'S'},
+	  { "supercapoff", 0, 0, 's'},
 	  { "help", 0, 0, 'h' },
 	  { 0, 0, 0, 0 }
 	};
@@ -163,9 +174,6 @@ int main(int argc, char **argv)
 
 	model = get_model();
 	switch(model) {
-	  case 0x7680:
-	  case 0x7682:
-	  case 0x7400:
 	  case 0x7553:
 		break;
 	  default:
@@ -180,30 +188,21 @@ int main(int argc, char **argv)
 	
 
 	while((c = getopt_long(argc, argv, 
-	  "iLFM:XYhm",
+	  "iL:hSs",
 	  long_options, NULL)) != -1) {
 		switch (c) {
 		  case 'i':
 			do_info(twifd);
 			break;
 		  case 'L':
-			opt_sleepmode = 1;
-			break;
-		  /* Standby is currently not supported by kernel*/
-		  /*case 'F':
-			opt_sleepmode = 2;
-			break;*/
-		  case 'M':
+			opt_sleep = 1;
 			opt_timewkup = strtoul(optarg, NULL, 0);
 			break;
-		  case 'm':
-			opt_resetswitchwkup = 1;
+		  case 'S':
+			opt_supercap = 1;
 			break;
-		  case 'X':
-			opt_resetswitch = 2;
-			break;
-		  case 'Y':
-			opt_resetswitch = 1;
+		  case 's':
+			opt_supercap = 2;
 			break;
 		  case 'h':
 		  default:
@@ -212,23 +211,21 @@ int main(int argc, char **argv)
 		}
 	}
 
-	if(opt_resetswitch) {
-		unsigned char dat = 0x40;	
-		
-		dat |= (opt_resetswitch & 0x2); //0x40 for off, 0x42 on
-		write(twifd, &dat, 1);
-	}
-
-	if(opt_sleepmode) {
+	if(opt_sleep) {
 		unsigned char dat[4] = {0};
 
-		dat[0]=(0x1 | (opt_resetswitchwkup << 1) |
-		  ((opt_sleepmode-1) << 4) | 1 << 6);
 		dat[3] = (opt_timewkup & 0xff);
 		dat[2] = ((opt_timewkup >> 8) & 0xff);
 		dat[1] = ((opt_timewkup >> 16) & 0xff);
+		dat[0] = ((opt_timewkup >> 24) & 0xff);
 		write(twifd, &dat, 4);
 	}
+
+	if(opt_supercap) {
+		unsigned char dat[1] = {(opt_supercap & 0x1)};
+		write(twifd, dat, 1);
+	}
+
 
 	
 	return 0;
