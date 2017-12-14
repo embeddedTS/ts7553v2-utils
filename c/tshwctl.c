56d2db973f5f8c1eeeef8807633bde8e70139560
Kris Bahnsen <kris@embeddedarm.com>
2017-12-14 10:33:55 -0800

    Updated tshwctl to be compatible with rev D Provides only model and board options

--- c/tshwctl.c
+++ c/tshwctl.c
@@ -1,4 +1,4 @@
-/*  Copyright 2016, Unpublished Work of Technologic Systems
+/*  Copyright 2017, Unpublished Work of Technologic Systems
 *  All Rights Reserved.
 *
 *  THIS WORK IS AN UNPUBLISHED WORK AND CONTAINS CONFIDENTIAL,
@@ -19,28 +19,30 @@
 /* To compile tshwctl, use the appropriate cross compiler and run the
 * command:
 *
-*  gcc -fno-tree-cselim -Wall -O0 -mcpu=arm9 -o tshwctl tshwctl.c gpiolib.c
+*  arm-linux-gnueabihf-gcc -fno-tree-cselim -Wall -O0 -o tshwctl tshwctl.c gpiolib.c
 *
 */
 
-#include <assert.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <dirent.h> 
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <getopt.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <errno.h>
-#include <stdint.h>
-#include <linux/types.h>
-#include <math.h>
-
 #include "gpiolib.h"
 
-static int twifd;
+#define STRAP0 87
+#define STRAP1 88
+#define STRAP2 91
+#define STRAP3 92
+
+const char copyright[] = "Copyright (c) Technologic Systems - " __DATE__ ;
+
+int model = 0;
 
 int get_model()
 {
@@ -50,221 +52,88 @@ int get_model()
 
 	proc = fopen("/proc/device-tree/model", "r");
 	if (!proc) {
-		perror("model");
-		return 0;
+	    perror("model");
+	    return 0;
 	}
 	fread(mdl, 256, 1, proc);
 	ptr = strstr(mdl, "TS-");
 	return strtoull(ptr+3, NULL, 16);
 }
 
-void auto485_en(int uart, int baud, char *mode)
+int do_info(void)
 {
-	unsigned int *mxpwmregs = NULL;
-	unsigned short speed;
-	int devmem;
+	uint8_t opts = 0;
+	int ret = 0;
 
-	devmem = open("/dev/mem", O_RDWR|O_SYNC);
-	mxpwmregs = mmap(0, getpagesize(), PROT_READ|PROT_WRITE,
-	  MAP_SHARED, devmem, 0x80064000);
-	//3_18
-	//mxgpioregs[0x178/4] = 0x3 << 4;
-	
-	speed = (24000000/((baud*1.275)-1));
-	mxpwmregs[0x8/4] = 0x3<<30;
-	mxpwmregs[0x4/4] = 1<<2;
-	mxpwmregs[0x50/4] = (((speed/2)<<16));
-	mxpwmregs[0x60/4] = (0x2<<18) | (0x3<<16) | speed;
+	ret += gpio_export(STRAP0);
+	ret += gpio_export(STRAP1);
+	ret += gpio_export(STRAP2);
+	ret += gpio_export(STRAP3);
 
-	munmap((void *)mxpwmregs, getpagesize());
-	close(devmem);
+	if (ret < 0) {
+		fprintf(stderr, "Unable to access GPIO pins for board opts!\n");
+	} else {
+		opts |= (gpio_read(STRAP0) << 0);
+		opts |= (gpio_read(STRAP1) << 1);
+		opts |= (gpio_read(STRAP2) << 2);
+		opts |= (gpio_read(STRAP3) << 3);
+
+		printf("model=%X\n", get_model());
+		printf("boardopt=%d\n", opts);
+	}
+
+	return ret;
+
 }
 
-
-void usage(char **argv) {
-	fprintf(stderr,
-		"Usage: %s [OPTIONS] ...\n"
-		"Technologic Systems I2C FPGA Utility\n"
-		"\n"
-		"  -i, --info             Display board info\n"
-		"  -x, --baud <speed>     Set baud rate for auto485\n"
-		"  -e, --cputemp          Print CPU internal temperature\n"
-		"  -p, --getmac           Display ethernet MAC address\n"
-		"  -l, --setmac=MAC       Set ethernet MAC address\n"
-		"  -h, --help             This message\n"
-		"\n",
-		argv[0]
+static void usage(char **argv) {
+	fprintf(stderr, "Usage: %s [OPTION] ...\n"
+	  "Technologic Systems Hardware access\n"
+	  "\n"
+	  "  -i, --info              Get info about the microcontroller\n"
+	  "  -h, --help              This message\n",
+	  argv[0]
 	);
 }
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
 	int c;
-	int opt_info = 0, opt_setmac = 0, opt_getmac = 0;
-	int opt_cputemp = 0;
-	char *opt_mac = NULL;
-	int baud = -1;
-	int model;
+	int ret;
 
 	static struct option long_options[] = {
-		{ "baud", 1, 0, 'x' },
-		{ "getmac", 0, 0, 'p' },
-		{ "setmac", 1, 0, 'l' },
-		{ "cputemp", 0, 0, 'e' },
-		{ "info", 0, 0, 'i' },
-		{ "help", 0, 0, 'h' },
-		{ 0, 0, 0, 0 }
+	  { "info", 0, 0, 'i' },
+	  { "help", 0, 0, 'h' },
+	  { 0, 0, 0, 0 }
 	};
 
+	if(argc == 1) {
+		usage(argv);
+		return(1);
+	}
+
 	model = get_model();
-	if(model != 0x7553) {
-		fprintf(stderr, "Unsupported model TS-%X\n", model);
+	switch(model) {
+	  case 0x7553:
+		break;
+	  default:
+		fprintf(stderr, "Unsupported model TS-%x\n", model);
 		return 1;
 	}
 
-	while((c = getopt_long(argc, argv, "x:hipl:e", long_options, NULL)) != -1) {
-		switch(c) {
-
-		case 'i':
-			opt_info = 1;
+	while((c = getopt_long(argc, argv, 
+	  "ih",
+	  long_options, NULL)) != -1) {
+		switch (c) {
+		  case 'i':
+			ret = do_info();
 			break;
-		case 'e':
-			opt_cputemp = 1;
-			break;
-		case 'x':
-			baud = atoi(optarg);
-			break;
-		case 'l':
-			opt_setmac = 1;
-			opt_mac = strdup(optarg);
-			break;
-		case 'p':
-			opt_getmac = 1;
-			break;
-		default:
+		  case 'h':
+		  default:
 			usage(argv);
 			return 1;
 		}
 	}
 
-	if(opt_info) {
-		printf("model=0x%X\n", model);
-		gpio_export(44);
-		printf("bootmode=0x%X\n", gpio_read(44) ? 1 : 0);
-	}
-
-	if(baud > -1) {
-		auto485_en(0, baud, NULL);
-	}
-
-	if (opt_cputemp) {
-		signed int temp[2] = {0,0}, x;
-		volatile unsigned int *mxlradcregs;
-		int devmem;
-
-		devmem = open("/dev/mem", O_RDWR|O_SYNC);
-		assert(devmem != -1);
-		mxlradcregs = (unsigned int *) mmap(0, getpagesize(),
-		  PROT_READ | PROT_WRITE, MAP_SHARED, devmem, 0x80050000);
-
-		mxlradcregs[0x148/4] = 0xFF;
-		mxlradcregs[0x144/4] = 0x98; //Set to temp sense mode
-		mxlradcregs[0x28/4] = 0x8300; //Enable temp sense block
-		mxlradcregs[0x50/4] = 0x0; //Clear ch0 reg
-		mxlradcregs[0x60/4] = 0x0; //Clear ch1 reg
-		temp[0] = temp[1] = 0;
-
-		for(x = 0; x < 10; x++) {
-			/* Clear interrupts
-			 * Schedule readings
-			 * Poll for sample completion
-			 * Pull out samples*/
-			mxlradcregs[0x18/4] = 0x3;
-			mxlradcregs[0x4/4] = 0x3;
-			while(!((mxlradcregs[0x10/4] & 0x3) == 0x3)) ;
-			temp[0] += mxlradcregs[0x60/4] & 0xFFFF;
-			temp[1] += mxlradcregs[0x50/4] & 0xFFFF;
-		}
-		temp[0] = (((temp[0] - temp[1]) * (1012/4)) - 2730000);
-		printf("internal_temp=%d.%d\n",temp[0] / 10000,
-		  abs(temp[0] % 10000));
-
-		munmap((void *)mxlradcregs, getpagesize());
-		close(devmem);
-	}
-
-	if (opt_setmac) {
-		/* This uses one time programmable memory. */
-		unsigned int a, b, c;
-		int r, devmem;
-		volatile unsigned int *mxocotpregs;
-
-		devmem = open("/dev/mem", O_RDWR|O_SYNC);
-		assert(devmem != -1);
-
-		r = sscanf(opt_mac, "%*x:%*x:%*x:%x:%x:%x",  &a,&b,&c);
-		assert(r == 3); /* XXX: user arg problem */
-
-		mxocotpregs = (unsigned int *) mmap(0, getpagesize(),
-		  PROT_READ | PROT_WRITE, MAP_SHARED, devmem, 0x8002C000);
-
-		mxocotpregs[0x08/4] = 0x200;
-		mxocotpregs[0x0/4] = 0x1000;
-		while(mxocotpregs[0x0/4] & 0x100) ; //check busy flag
-		if(mxocotpregs[0x20/4] & (0xFFFFFF)) {
-			printf("MAC address previously set, cannot set\n");
-		} else {
-			assert(a < 0x100);
-			assert(b < 0x100);
-			assert(c < 0x100);
-			mxocotpregs[0x0/4] = 0x3E770000;
-			mxocotpregs[0x10/4] = (a<<16|b<<8|c);
-		}
-		mxocotpregs[0x0/4] = 0x0;
-
-		munmap((void *)mxocotpregs, getpagesize());
-		close(devmem);
-	}
-
-	if (opt_getmac) {
-		unsigned char a, b, c;
-		unsigned int mac;
-		int devmem;
-		volatile unsigned int *mxocotpregs;
-
-		devmem = open("/dev/mem", O_RDWR|O_SYNC);
-		assert(devmem != -1);
-		mxocotpregs = (unsigned int *) mmap(0, getpagesize(),
-		  PROT_READ | PROT_WRITE, MAP_SHARED, devmem, 0x8002C000);
-
-		mxocotpregs[0x08/4] = 0x200;
-		mxocotpregs[0x0/4] = 0x1000;
-		while(mxocotpregs[0x0/4] & 0x100) ; //check busy flag
-		mac = mxocotpregs[0x20/4] & 0xFFFFFF;
-		if(!mac) {
-			mxocotpregs[0x0/4] = 0x0; //Close the reg first
-			mxocotpregs[0x08/4] = 0x200;
-			mxocotpregs[0x0/4] = 0x1013;
-			while(mxocotpregs[0x0/4] & 0x100) ; //check busy flag
-			mac = (unsigned short) mxocotpregs[0x150/4];
-			mac |= 0x4f0000;
-		}
-		mxocotpregs[0x0/4] = 0x0;
-
-		a = mac >> 16;
-		b = mac >> 8;
-		c = mac;
-		
-		printf("mac=00:d0:69:%02x:%02x:%02x\n", a, b, c);
-		printf("shortmac=%02x%02x%02x\n", a, b, c);
-
-		munmap((void *)mxocotpregs, getpagesize());
-		close(devmem);
-	}
-
-
-	close(twifd);
-
-	return 0;
+	return (ret ? 1 : 0);
 }
-
